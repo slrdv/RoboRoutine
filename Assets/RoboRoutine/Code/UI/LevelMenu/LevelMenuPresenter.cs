3ba42b982283aaa906198e477f3bf83d53865d3e
slrdv <slrdv@yandex.com>
2026-07-11 14:40:49 +0300

    Fix LevelMenuPresenter subscriptions

--- Assets/RoboRoutine/Code/UI/LevelMenu/LevelMenuPresenter.cs
+++ Assets/RoboRoutine/Code/UI/LevelMenu/LevelMenuPresenter.cs
@@ -33,7 +33,7 @@ namespace RoboRoutine
 
         public void Dispose()
         {
-            _inputHandler.IgmActionEvent += ToggleShow;
+            _inputHandler.IgmActionEvent -= ToggleShow;
 
             _view.NextButtonPressedEvent -= OnNextButtonPressed;
             _view.MenuButtonPressedEvent -= OnMenuButtonPressed;
